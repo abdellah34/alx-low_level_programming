16837d30ae49fa1bdf3a399ddb63e73d8382f90d
abdellah34 <aammari.1989@gmail.com>
2023-07-18 10:31:07 -0700

    I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+void print_alphabet(void)
+{
+	char letter ;
+	for (letter = 'a' ; letter <= 'z' ;letter++)
+	{_putchar(letter);
+	}
+	_putchar('\n');
+}
