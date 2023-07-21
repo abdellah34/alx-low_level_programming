faa8e85933eab4b68a050ceb91c4b2316394ff75
abdellah34 <aammari.1989@gmail.com>
2023-07-20 17:18:35 -0700

    9. Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+		{
+			if ((i % 3 == 0) && (i % 5 == 0))
+			{
+				printf("FizzBuzz ");
+			}
+			else if (i % 3 == 0)
+			{
+				printf("Fizz ");
+			}
+			else if (i % 5 == 0)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("%d ", i);
+			}
+		}
+	printf("\n");
+	return (0);
+}
