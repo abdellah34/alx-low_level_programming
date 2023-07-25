aa746c81ed2194aafca256dd3469731b9c848104
abdellah34 <aammari.1989@gmail.com>
2023-07-24 19:17:03 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,19 +7,14 @@
 */
 char *_strcpy(char *dest, char *src)
 {
-	int n = 0, i;
+	char *ori = dest;
 
-	while (src[n] != '\0')
+	while (*src != '\0')
 	{
-		n++;
+		*dest = *src;
+		dest++;
+		src++;
 	}
-	for (i = 0; i < n && src[i] != '\0'; i++)
-	{
-		dest[i] = src[i];
-	}
-	for ( ; i < n; i++)
-	{
-		dest[i] = '\0';
-	}
-	return (dest);
+	*dest = '\0';
+	return (ori);
 }
