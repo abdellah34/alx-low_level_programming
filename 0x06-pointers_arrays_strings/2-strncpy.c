43fff4ec94435165eb8b0aa557a3550605031d5d
abdellah34 <aammari.1989@gmail.com>
2023-07-26 12:38:36 -0700

    copie a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* *_strncpy -  copies the string pointed to by src
+* @dest: a string pointer
+* @src: a string pointer
+* @n: the number of bytes from src
+*Return: the poiter to dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = *src;
+		i++;
+		src++;
+	}
+	return (dest);
+}
