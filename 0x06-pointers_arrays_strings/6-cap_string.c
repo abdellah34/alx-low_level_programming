e8ebc60273d3bf2726f090c8b66a9d88b69c23e3
abdellah34 <aammari.1989@gmail.com>
2023-07-28 17:48:58 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,34 +6,20 @@
 */
 char *cap_string(char *a)
 {
-	int i, j, count = 0;
+	int i, j;
 	char p[] = "\n  ,.\t;!?\"(){}";
 
 	for (i = 0; a[i] != '\0'; i++)
 	{
-		if (a[i] == '\t')
-		{
-			a[i] = ' ';
-		}
-	}
-	for (i = 0; a[i] != '\0'; i++)
-	{
-		for (j = 0; p[j] != '\0'; j++)
-		{
-			if ((a[i] == 0) || a[i - 1] == p[j])
+		if (a[i] >= 'a' && a[i] <= 'z')
+			for (j = 0; p[j] != '\0'; j++)
 			{
-				count = 1;
+				if ((a[i] == 0) || a[i - 1] == p[j])
+				{
+					a[i] = a[i] - 32;
+					break;
+				}
 			}
-			else if (a[i] >= 'a' && a[i] <= 'z' && count)
-			{
-				a[i] = a[i] - 32;
-			}
-			else
-			{
-				a[i] = a[i];
-			}
-		}
-		count = 0;
 	}
 	return (a);
 }
