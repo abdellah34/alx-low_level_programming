f8e963575b428cf0b71da508923751f6e49523cf
abdellah34 <aammari.1989@gmail.com>
2023-08-01 18:22:10 -0700

    the sums of twodigonals

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -3,20 +3,21 @@
 /**
 * print_diagsums - prints the sum of the two diagonals of a square matrix
 * @a: is the matrix
+* @size: the size of the matrix
 * Does returns anything
 */
 void print_diagsums(int *a, int size)
 {
 	int i;
-	int sum1 = 0,sum2 = 0;
+	int sum1 = 0, sum2 = 0;
 
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-		sum1 += *(a + i *size + i);
+		sum1 += *(a + i * size + i);
 	}
-	for(i = 0; i < size; i++)
-        {
-                sum2 += *(a + i *size + (size -1 - i));
-        }
+	for (i = 0; i < size; i++)
+	{
+		sum2 += *(a + i * size + (size - 1 - i));
+	}
 	printf("%d, %d\n", sum1, sum2);
 }
