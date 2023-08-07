79f39c46ce91f93d7d9c90f5e0e911016fb1da73
abdellah34 <aammari.1989@gmail.com>
2023-08-07 08:39:51 -0700

    all arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * @argv: argument vector
+ *@argc: argument count
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
