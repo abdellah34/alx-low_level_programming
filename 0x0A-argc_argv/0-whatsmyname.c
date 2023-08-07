138819f231941f43b2ec59409fc3a373de7fde73
abdellah34 <aammari.1989@gmail.com>
2023-08-07 08:01:46 -0700

    my name is

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * @argv: argument vector
+ *@argc: argument count
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
