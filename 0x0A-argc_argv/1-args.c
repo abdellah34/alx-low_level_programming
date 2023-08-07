2ebe4e8080719712e7d486625184bdce93273e60
abdellah34 <aammari.1989@gmail.com>
2023-08-07 08:24:08 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * @argv: argument vector
+ *@argc: argument count
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
