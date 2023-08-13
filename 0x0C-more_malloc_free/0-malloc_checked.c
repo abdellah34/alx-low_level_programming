80923c88580c751308a97ebae162fe680f685aa3
abdellah34 <aammari.1989@gmail.com>
2023-08-13 10:44:51 -0700

    trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,14 +3,15 @@
 /**
  *malloc_checked -  alocates memory using malloc
  *@b: The seize
- *Doses retun anything
+ *Return: a pointer
  */
 void *malloc_checked(unsigned int b)
 {
-	void *ptr = malloc (b);
+	void *ptr = malloc(b);
+
 	if (ptr == NULL)
 	{
-		exit (98);
+		exit(98);
 	}
 	return (ptr);
 }
