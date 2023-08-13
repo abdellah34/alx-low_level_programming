bf0fc23b19f9c5ce998be3febb08ff0cf74ebf8f
abdellah34 <aammari.1989@gmail.com>
2023-08-13 15:44:47 -0700

    my calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,10 +9,11 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int tot = nmemb * size, k;
-	void *ptr = malloc(tot);
+	void *ptr = NULL;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
+	ptr = malloc(tot);
 	if (ptr == NULL)
 		return (NULL);
 	for (k = 0; k < tot; k++)
