2b2019bc0def15b63f62299304b2901e9c00a2ef
abdellah34 <aammari.1989@gmail.com>
2023-08-14 10:02:13 -0700

    print struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 	{
 		if ((*d).name == NULL)
 		{
-			printf("Name: nil\n");
+			printf("Name: (nil)\n");
 		}
 		else
 		{
@@ -22,7 +22,7 @@ void print_dog(struct dog *d)
 
 		if ((*d).owner == NULL)
 		{
-			printf("Owner: nil\n");
+			printf("Owner: (nil)\n");
 		}
 		else
 		{
