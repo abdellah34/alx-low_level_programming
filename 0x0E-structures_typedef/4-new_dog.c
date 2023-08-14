401a9e55e8835d24eeb320029a76e04bc3ca10be
abdellah34 <aammari.1989@gmail.com>
2023-08-14 12:28:08 -0700

    create a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * str_len - calculates the length of a string.
+ * @s: the string.
+ * Return: the length.
+ */
+unsigned int str_len(char *s)
+{
+	unsigned int len = 0;
+
+	while (*s++)
+		len++;
+	return (len);
+}
+/**
+ * new_dog - creates a new dog.
+ * @name: name .
+ * @age: age.
+ * @owner: Owner.
+ * Return: a new dog's infos.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *n_dog;
+	unsigned int i;
+	unsigned int n_len = str_len(name);
+	unsigned int o_len = str_len(owner);
+
+	n_dog = (dog_t *)malloc(sizeof(dog_t));
+	if (n_dog == NULL)
+	{
+		return (NULL);
+	}
+	n_dog->name = (char *)malloc(n_len + 1);
+
+	if (n_dog->name == NULL)
+	{
+		free(n_dog);
+		return (NULL);
+	}
+	n_dog->owner = (char *)malloc(o_len + 1);
+
+	if (n_dog->owner == NULL)
+	{
+		free(n_dog);
+		return (NULL);
+	}
+	for (i = 0; i <= n_len; i++)
+	{
+		n_dog->name[i] = name[i];
+	}
+	for (i = 0; i <= o_len; i++)
+	{
+		n_dog->owner[i] = owner[i];
+	}
+	n_dog->age = age;
+	return (n_dog);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,9 +14,10 @@ struct dog
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
 
