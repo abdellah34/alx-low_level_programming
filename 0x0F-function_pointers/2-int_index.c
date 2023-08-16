8dcbbde58a2ded32e91424c11322b68fbfb7e83a
abdellah34 <aammari.1989@gmail.com>
2023-08-16 11:39:54 -0700

    searche an integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,18 +12,19 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int index = 0;
 
-	if (array == NULL || size <= 0)
+	if (array == NULL || cmp == 0)
 	{
 		return (-1);
 	}
-	if (cmp != NULL)
+	if (size <= 0)
 	{
-		while (index < size)
-		{
-			if (cmp(array[index]))
-				break;
-			index++;
-		}
+		return (-1);
+	}
+	while (index < size)
+	{
+		if (cmp(array[index]))
+			break;
+		index++;
 	}
 	return (index);
 }
