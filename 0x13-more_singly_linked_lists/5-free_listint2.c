9e1c0834012f03cf2b35292eec3c86e6026a62d3
abdellah34 <aammari.1989@gmail.com>
2023-08-29 07:02:19 -0700

    free list 2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,13 +8,15 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *curr = *head;
-	listint_t *aux; 
+	listint_t *aux;
 
+	if (!head)
+		return;
 	while (curr)
 	{
 		aux =  curr;
 		curr = curr->next;
 		free(aux);
+		*head = NULL;
 	}
-	*head = NULL;
 }
