ffce7d078c06c6a5fd1ec0a9e11a01065f8d3252
abdellah34 <aammari.1989@gmail.com>
2023-08-30 16:25:07 -0700

    insert a new node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * insert_nodeint_at_index - insert a new node at a given position.
  * @head: the head node .
@@ -11,36 +10,38 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node;
 	listint_t *curr = *head;
-	unsigned int i = 1;
+	unsigned int len = 0;
 
 	if (head == NULL)
 		return (NULL);
-
 	new_node = (listint_t *)malloc(sizeof(listint_t));
 	if (!new_node)
 		return (NULL);
 	new_node->n = n;
 	new_node->next = NULL;
-
-	if (idx == 0)
+	while (curr)
 	{
-		new_node->next = *head;
-		*head = new_node;
-	}
-
-	while (curr && i < idx - 1)
-	{
-		i++;
+		len++;
 		curr = curr->next;
 	}
-
-	if (curr == NULL)
+	if (idx > len)
 	{
 		free(new_node);
 		return (NULL);
 	}
-	new_node->next = curr->next;
-	curr->next = new_node;
-
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+	else
+	{
+		curr = *head;
+		while (--idx)
+			curr = curr->next;
+		new_node->next = curr->next;
+		curr->next = new_node;
+	}
 	return (new_node);
 }
