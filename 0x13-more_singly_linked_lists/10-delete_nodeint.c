8cfdef3e0c4417c9c0dd21122c668458142c70ef
abdellah34 <aammari.1989@gmail.com>
2023-08-29 18:24:54 -0700

    delete node

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,35 @@
+{
+	listint_t *hold, *delete;
+	unsigned int i, action = 0;
+
+	if (!*head)
+		return (-1);
+	for (i = 0, hold = *head; hold && index; i++, hold = (*hold).next)
+		if (i == (index - 1))
+		{
+			action = 1;
+			break;
+		}
+
+	if (action)
+	{
+		delete = (*hold).next;
+		(*hold).next = (*delete).next;
+		free(delete);
+		return (1);
+	}
+	else if (!index && (**head).next)
+	{
+		delete = *head;
+		*head = (*delete).next;
+		free(delete);
+		return (1);
+	}
+	else if (!index && *head)
+	{
+		*head = NULL;
+		free(*head);
+		return (1);
+	}
+	return (-1);
+}
