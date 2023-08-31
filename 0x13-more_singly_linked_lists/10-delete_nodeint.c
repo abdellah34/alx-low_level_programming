196c0a47381e7f4af8b3b49de503b38bc301455a
abdellah34 <aammari.1989@gmail.com>
2023-08-30 17:19:09 -0700

    delete node at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,37 +1,42 @@
 #include "lists.h"
-
 /**
- * delete_nodeint_at_index - function deletes node at given position
- *
- *@head: pointer to pointer of head of linked list
- *@index: unsigned int index position
- *
- * Return: 1 if successful, -1 if failed
- */
-
+ * delete_nodeint_at_index - insert a new node at a given position.
+ * @head: the head node .
+ * @index: the index of the node should be deleted.
+ * Return: 1 if it succeeded, -1 if it failed.
+ **/
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *temp, *next;
-	unsigned int i;
+	listint_t *curr = *head, *p = *head;
+	unsigned int len = 0;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return (-1);
-
-	temp = *head;
-
+	while (curr)
+	{
+		len++;
+		curr = curr->next;
+	}
+	if (index > len)
+	{
+		return (-1);
+	}
 	if (index == 0)
 	{
-		*head = temp->next;
-		free(temp);
-		return (1);
+		*head = (*head)->next;
+		free(p);
 	}
-	for (i = 0; temp != NULL && i < index - 1; i++)
-		temp = temp->next;
-	if (temp == NULL || temp->next == NULL)
-		return (-1);
-
-	next = temp->next->next;
-	free(temp->next);
-	temp->next = next;
+	else
+	{
+		curr = *head;
+		while (--index)
+		{
+			curr = curr->next;
+			p = p->next;
+		}
+		p = p->next;
+		curr->next = p->next;
+		free(p);
+	}
 	return (1);
 }
