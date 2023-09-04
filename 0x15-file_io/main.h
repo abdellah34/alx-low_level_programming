fbf8d1965e60fb6cc57acfaa29ccada5f844904f
abdellah34 <aammari.1989@gmail.com>
2023-09-04 16:47:00 -0700

    header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stddef.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
